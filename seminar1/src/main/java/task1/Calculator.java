cfff58af713e7af3cb4901777bd23215f5305fe4
David <david@akunts.ru>
2023-11-02 23:35:30 +0300

    hw1

---
+++ seminar1/src/main/java/task1/Calculator.java
@@ -0,0 +1,31 @@
+package task1;
+
+public class Calculator {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public double divide(int a, int b) {
+        if (b == 0) {
+            throw new IllegalArgumentException("Divider cannot be zero");
+        }
+        return (double) a / b;
+    }
+
+    public double calculateDiscount(double purchaseAmount, double discountPercentage) {
+        if (purchaseAmount < 0 || discountPercentage < 0 || discountPercentage > 100) {
+            throw new ArithmeticException("Invalid input values");
+        }
+        double discountAmount = purchaseAmount * (discountPercentage / 100);
+        return purchaseAmount - discountAmount;
+    }
+}
